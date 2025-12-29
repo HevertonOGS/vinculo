8adb4949658e5516c2e2be792e412870d97515e3
HevertonOGS <heverton.ogs@gmail.com>
2025-12-29 14:06:13 -0300

    chore: initial setup from nest

--- apps/app/jest.config.cts
+++ apps/app/jest.config.cts
@@ -11,7 +11,7 @@ const config = {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/vinculo',
+  coverageDirectory: '../../coverage/apps/app',
   testEnvironment: 'jsdom',
 };
 
--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,6 +1,18 @@
+'use client';
 import styles from './page.module.scss';
 
+import { useEffect, useState } from 'react';
+
 export default function Index() {
+  const [message, setMessage] = useState('Carregando...');
+
+  useEffect(() => {
+    fetch('http://localhost:3333/api')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => setMessage('Erro ao conectar com API'));
+  }, []);
+
   return (
     <div className={styles.page}>
       <div className={styles.wrapper}>
@@ -10,9 +22,12 @@ export default function Index() {
               <h3 className={styles.subtitle}>Bem-vindo ao</h3>
               <h1 className={styles.title}>Vínculo App</h1>
             </div>
+            <div>{message}</div>
             <div className={styles.actions}>
               <button className={styles.button}>Iniciar jornada</button>
-              <button className={styles.buttonSecondary}>Já tenho uma conta</button>
+              <button className={styles.buttonSecondary}>
+                Já tenho uma conta
+              </button>
             </div>
           </div>
         </div>
